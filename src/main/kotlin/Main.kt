cd4d933c11234eb0523f530075065ae9342edce3
Klenam <57183832+kekele-star@users.noreply.github.com>
2021-08-12 18:37:06 +0000

    Kotlin initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,18 @@
-
+/*Classes, Objects and Instances in Koltin */
 
 fun main(args: Array<String>) {
      val kelly = Player("kelly")
-     println(kelly.name)
-     println(kelly.lives)
-     println(kelly.score)
+//     println(kelly.name)
+//     println(kelly.lives)
+//     println(kelly.score)
+    kelly.show()
+
+    val louise = Player("Louise",5)
+    louise.show()
+
+
+    val bao = Player("Bao", 4,8)
+    val grace = Player("Grace",2,5,1000)
+    bao.show()
+    grace.show()
  }
