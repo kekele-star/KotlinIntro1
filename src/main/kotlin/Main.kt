880005e1ee911f18f1698081616a9b403aa01bf2
Klenam <57183832+kekele-star@users.noreply.github.com>
2021-08-12 15:05:38 +0000

    Kotlin initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+
+
+fun main(args: Array<String>) {
+     val kelly = Player("kelly")
+     println(kelly.name)
+     println(kelly.lives)
+     println(kelly.score)
+ }
---
+++ src/main/kotlin/Player.kt
@@ -0,0 +1,7 @@
+
+
+class Player(val name: String) {
+    var lives = 3
+    var level = 1
+    var score = 0
+}
